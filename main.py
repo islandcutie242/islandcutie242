5d793086989022a9d976276de1548ce52ad54aeb
islandcutie242 <rpdarling@hotmail.com>
2022-04-11 00:06:14 -0400

    working with classes python

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
-def greeting():
-    print("Hello what is your name? ")
-    name = input();
-    print(f"Hello {name}!")
+class Robot:
+    # creating a custom constructor
+    def __init__(self, name, color, weight):
+        self.name = name
+        self.color = color
+        self.weight = weight
+    
+    
+    def introduce_self(self):
+        print("Hello my name is "+ self.name)
+    
+# r1 = Robot()
+# r1.name = "Tim"
+# r1.color = "silver"
+# r1.weight = 40
 
-# greeting()
+#r2 = Robot()
+#r2.name = "Sara"
+# r2.color = "bronze"
+# r2.weight = 34
+
+r1 = Robot("Tim","silver", 56)
+r2 = Robot("Sara","bronze", 34)
+
+r2.introduce_self()
